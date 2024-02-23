11ff0bbb63757aaddeb8504ae4915c3ce5197a69
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-02-23 10:07:39 +0700

    green build

--- elabs-backend/src/EA/Api/Order.hs
+++ elabs-backend/src/EA/Api/Order.hs
@@ -5,9 +5,6 @@ module EA.Api.Order (
 
 import Data.Aeson qualified as Aeson
 import Data.Swagger qualified as Swagger
-import EA (EAApp, eaAppEnvGYNetworkId, eaMarketplaceAtTxOutRef)
-import EA.Api.Types (SubmitTxResponse, UserId)
-import GeniusYield.Types (GYTxOutRef)
 import Servant (
   Capture,
   GenericMode ((:-)),
@@ -20,7 +17,34 @@ import Servant (
   type (:>),
  )
 import Servant.Swagger (HasSwagger (toSwagger))
-
+import EA.Script.Marketplace(MarketplaceParams(..), MarketplaceInfo(..))
+import EA (
+  EAApp,
+  EAAppEnv (eaAppEnvGYNetworkId, eaAppEnvScripts, eaAppEnvGYProviders),
+  eaLiftMaybe,
+  eaSubmitTx,
+  eaMarketplaceAtTxOutRef, 
+  eaOracleAtTxOutRef,
+ )
+import EA.Api.Types (SubmitTxResponse, UserId)
+import EA.Script (nftMintingPolicy, oracleValidator)
+import EA.Wallet (
+  eaGetAddresses,
+  eaGetCollateralFromInternalWallet,
+  eaGetInternalAddresses,
+  eaSelectOref,
+ )
+import GeniusYield.TxBuilder (runGYTxMonadNode)
+import GeniusYield.Types (
+  GYAssetClass (GYToken),
+  unsafeTokenNameFromHex,
+  validatorHash,
+  GYTxOutRef, 
+  addressToPubKeyHash, 
+  mintingPolicyId
+ )
+import Internal.Wallet qualified as Wallet
+import EA.Tx.Changeblock.Marketplace(buy)
 --------------------------------------------------------------------------------
 
 data OrderApi mode = OrderApi
@@ -82,7 +106,7 @@ data OrderRequest = OrderRequest
   deriving anyclass (Aeson.FromJSON, Swagger.ToSchema)
 
 data OrderBuyRequest = OrderBuyRequest
-  { userId :: !UserId
+  { buyerId :: !UserId
   -- ^ The user ID.
   , amount :: !Int
   -- ^ The amount of carbon to buy.
@@ -101,11 +125,69 @@ handleOrderCreate = error "TODO"
 
 handleOrderBuy :: OrderBuyRequest -> EAApp SubmitTxResponse
 handleOrderBuy orderRequest = do
-  _nid <- asks eaAppEnvGYNetworkId
+  nid <- asks eaAppEnvGYNetworkId
+  providers <- asks eaAppEnvGYProviders
   _marketplaceInfo <- eaMarketplaceAtTxOutRef $ orderId orderRequest
-  -- let tx = buy
-  --           nid
-  --           marketplaceInfo
+  _scripts <- asks eaAppEnvScripts
+  -- TODO:
+  _oracleInfo <- eaOracleAtTxOutRef $ orderId orderRequest
+
+  -- Get the internal address pairs.
+  internalAddrPairs <- eaGetInternalAddresses False
+
+  -- Get the user address. We don't need the signing key here.
+  (_userAddr, _) <-
+    eaLiftMaybe "No addresses found"
+      . listToMaybe
+      =<< eaGetAddresses (buyerId orderRequest)
+
+  -- Get the collateral address and its signing key.
+  (collateral, colKey) <-
+    eaGetCollateralFromInternalWallet >>= eaLiftMaybe "No collateral found"
+
+  (addr, key, oref) <-
+    eaSelectOref
+      internalAddrPairs
+      (\r -> collateral /= Just (r, True))
+      >>= eaLiftMaybe "No UTxO found"
+
+  _buyer <- eaLiftMaybe "Cannot decode address" (addressToPubKeyHash addr)
+
+  -- TODO: User proper policyId for Oracle NFT
+  let oracleNftAsset = mintingPolicyId $ nftMintingPolicy oref _scripts
+      oracleNftAssetName = unsafeTokenNameFromHex "43424c"
+      orcAssetClass = GYToken oracleNftAsset oracleNftAssetName
+
+      -- TODO: user proper operaor pubkey hash for oracle validator
+      orcValidatorHash =
+        validatorHash $ oracleValidator orcAssetClass (mktInfoIssuer _marketplaceInfo) _scripts
+
+      _marketParams =
+        MarketplaceParams
+          { mktPrmOracleValidator = orcValidatorHash
+          , mktPrmEscrowValidator = mktInfoIssuer _marketplaceInfo
+          , -- \^ TODO: User proper pubkeyhash of escrow
+            mktPrmVersion = unsafeTokenNameFromHex "76312e302e30"
+          , -- \^ It can be any string for now using v1.0.0
+            mktPrmOracleSymbol = oracleNftAsset
+          , mktPrmOracleTokenName = oracleNftAssetName
+          }
+  -- TODO:
+  let _mMarketplaceRefScript = Just (orderId orderRequest)
+  let tx = buy
+            nid
+            _marketplaceInfo
+            _oracleInfo
+            _buyer
+            _mMarketplaceRefScript
+            _marketParams
+            _scripts
+
+  txBody <-
+    liftIO $
+      runGYTxMonadNode nid providers [addr] addr collateral (return tx)
+
+  void $ eaSubmitTx $ Wallet.signTx txBody [key, colKey]
   error "TODO"
 
 handleOrderCancel :: Int -> EAApp SubmitTxResponse
